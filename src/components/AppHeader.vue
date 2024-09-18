663061237bd8ec0d4b58e9ad420ea81bcbac7e2f
Angelo.Crisci <angelocrisci1998@gmail.com>
2024-09-18 16:43:36 +0200

    first commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    data() {
+        return {
+            name: 'AppHeader',
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>
+        Yu-Gi-Oh API
+    </h1>
+
+</template>
+
+<style scoped lang="scss">
+h1 {
+    color: black;
+    padding: 20px;
+}
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    data() {
+        return {
+            name: 'AppMain',
+        }
+    }
+}
+</script>
+
+<template>
+
+
+</template>
+
+<style scoped lang="scss"></style>
